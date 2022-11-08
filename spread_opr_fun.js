7a55eb8817ff2986e55614340327749e4ec13225
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-11-08 18:07:37 +0530

    Update spread_opr_fun.js

--- spread_opr_fun.js
+++ spread_opr_fun.js
@@ -3,3 +3,12 @@ function sum(a,b,c){
 }
 let num=[1,2,3]
 console.log(sum(...num))
+
+
+
+//ignore condition
+function sum(a,b,c){
+    return a+b+c;
+}
+let num=[1,2,3,4,5,6]
+console.log(sum(...num))
