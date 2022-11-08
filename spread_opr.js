698d80cfff9660a158467fca5896f511ef4382f3
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-11-08 17:54:43 +0530

    Create spread_opr.js

---
+++ spread_opr.js
@@ -0,0 +1,11 @@
+arr=[1,2,3,4,5]
+arr2=[...arr]
+console.log(arr2)
+
+
+let person={name:"manohar",role:"forntend",salary:300000}
+let person2={name:"joshi",role:"android",salary:450000}
+let person3={...person2}
+console.log(person3)
+let person4={...person}
+console.log(person4)
