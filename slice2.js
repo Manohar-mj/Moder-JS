382516d2b38f023a3a8c5b02765a93825c54350e
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-11-12 23:50:43 +0530

    Create slice2.js

---
+++ slice2.js
@@ -0,0 +1,7 @@
+//slice create new form source array and it doesn't remove any element from source array
+const arr=["game","app","web","design"]
+const newarr=arr.slice(2) //web and design
+const thiarr=arr.slice(3) //design
+console.log(arr)
+console.log(newarr)
+console.log(thiarr)
