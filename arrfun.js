2b4389f02f7c1a1fddfdd22beadb9a22c9ad7a83
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-11-12 23:07:47 +0530

    Create arrfun.js

---
+++ arrfun.js
@@ -0,0 +1,9 @@
+let arr2=[11,22,33,44,55]
+arr2.push(66) //add new number at end
+console.log(arr2)
+
+arr2.pop() // remove last number 
+console.log(arr2)
+
+arr2.shift() // remove first number
+console.log(arr2)
